8d7b7ffda261860db3458b40d3a00913e0625508
Romina Acuña <romina.24ar@gmail.com>
2015-02-11 10:17:08 -0300

    topic6 example

---
+++ Topic6 Example/MiServicioREST/src/main/java/com/globant/topic6/Persona.java
@@ -0,0 +1,35 @@
+package com.globant.topic6;
+
+public class Persona {
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	public Persona(){}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
